c8a0680a664e5b0aa4e05003d1c9428ef8fd7cfd
alonapp7 <89524241+alonapp7@users.noreply.github.com>
2021-11-03 12:33:46 +0200

    Update processor.rs

--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,7 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
+    program_error::ProgramError,
     msg,
     pubkey::Pubkey,
 };
@@ -18,5 +19,25 @@ impl Processor {
                 Self::process_init_escrow(accounts, amount, program_id)
             }
         }
+    }
+    fn process_init_escrow(
+        accounts: &[AccountInfo],
+        amount: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let initializer = next_account_info(account_info_iter)?;
+
+        if !initializer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+        let temp_token_account = next_account_info(account_info_iter)?;
+
+        let token_to_receive_account = next_account_info(account_info_iter)?;
+        if *token_to_receive_account.owner != spl_token::id() {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        Ok(())
     }
 }
