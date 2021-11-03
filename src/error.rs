e0744043e964ce234c24b8bf8f8ca092ea544ae3
alonapp7 <89524241+alonapp7@users.noreply.github.com>
2021-11-03 12:16:53 +0200

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,10 @@
 
+// inside error.rs
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
